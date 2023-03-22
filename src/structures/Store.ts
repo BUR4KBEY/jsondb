c5fc14aa98482b28afe7bf30664bbb77ee30962f
BUR4KBEY <rectfc@gmail.com>
2023-03-22 11:50:01 +0300

    feat(): create files and folder if does not exist

--- src/structures/Store.ts
+++ src/structures/Store.ts
@@ -1,4 +1,4 @@
-import { readFileSync, writeFileSync } from 'fs';
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs';
 import { join } from 'path';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -8,17 +8,22 @@ import Item from './Item';
 export default class Store {
     mainPath: string;
 
-    constructor(
-        options: Pick<Store, 'mainPath'> & Partial<Omit<Store, 'folderPath'>>
-    ) {
+    constructor(options: Pick<Store, 'mainPath'>) {
         Object.assign(this, options);
+
+        if (!existsSync(options.mainPath)) {
+            mkdirSync(options.mainPath);
+        }
     }
 
     fetch<T>(collection: Collection<Item<unknown>>): T {
-        const dataStr = readFileSync(
-            join(this.mainPath, `${collection.name}.json`),
-            'utf-8'
-        );
+        const filePath = join(this.mainPath, `${collection.name}.json`);
+
+        if (!existsSync(filePath)) {
+            writeFileSync(filePath, '[]');
+        }
+
+        const dataStr = readFileSync(filePath, 'utf-8');
         const data = JSON.parse(dataStr);
         return data;
     }
